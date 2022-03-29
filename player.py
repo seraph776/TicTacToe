ac4f586ad21485ad44cbaeda09a677c4f3288446
seraph776 <seraph776@gmail.com>
2022-03-29 17:13:01 -0400

    Updating player.py

--- player.py
+++ player.py
@@ -10,7 +10,7 @@ import math
 import random
 
 
-class Player:
+class Player():
     def __init__(self, letter):
         self.letter = letter
 
@@ -26,7 +26,7 @@ class HumanPlayer(Player):
         valid_square = False
         val = None
         while not valid_square:
-            square = input(f"{self.letter}'s turn. Input move (0-9): ")
+            square = input(self.letter + '\'s turn. Input move (0-9): ')
             try:
                 val = int(square)
                 if val not in game.available_moves():
@@ -54,44 +54,37 @@ class SmartComputerPlayer(Player):
         if len(game.available_moves()) == 9:
             square = random.choice(game.available_moves())
         else:
-            square = self.minmax(game, self.letter)['position']
+            square = self.minimax(game, self.letter)['position']
         return square
 
-    def minmax(self, state, player):
-        max_player = self.letter
+    def minimax(self, state, player):
+        max_player = self.letter  # yourself
         other_player = 'O' if player == 'X' else 'X'
 
-        # First we want to check if the previous move is a winner
+        # first we want to check if the previous move is a winner
         if state.current_winner == other_player:
-            return {'position': None, 'score': 1 * (state.num_empty_squares() + 1) if other_player == max_player else -1 * (state.num_empty_squares() + 1)}
+            return {'position': None, 'score': 1 * (state.num_empty_squares() + 1) if other_player == max_player else -1 * (
+                        state.num_empty_squares() + 1)}
         elif not state.empty_squares():
             return {'position': None, 'score': 0}
 
         if player == max_player:
-            best = {'position': None, 'score': -math.inf}
+            best = {'position': None, 'score': -math.inf}  # each score should maximize
         else:
-            best = {'position': None, 'score': -math.inf}
+            best = {'position': None, 'score': math.inf}  # each score should minimize
         for possible_move in state.available_moves():
             state.make_move(possible_move, player)
-            sim_score = self.minmax(state, other_player)
+            sim_score = self.minimax(state, other_player)  # simulate a game after making that move
 
-            state.board[possible_move] = ''
+            # undo move
+            state.board[possible_move] = ' '
             state.current_winner = None
-            sim_score['position'] = possible_move
+            sim_score['position'] = possible_move  # this represents the move optimal next move
 
-            if player == max_player:
+            if player == max_player:  # X is max player
                 if sim_score['score'] > best['score']:
                     best = sim_score
-                else:
-                    if sim_score['score'] < best['score']:
-                        best = sim_score
-            return best
-
-
-
-
-
-
-
-
-
+            else:
+                if sim_score['score'] < best['score']:
+                    best = sim_score
+        return best
